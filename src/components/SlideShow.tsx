ea4137d0a21aa2707c32666423058d83844bf10a
Pierre.WEBER <wbrp51@gmail.com>
2024-05-07 12:35:50 +0200

    feat(SlideShow): hide navigation and counter for single image
    Implemented a conditional check in the SlideShow component to hide the
    previous/next navigation buttons and image counter when there is only
    one image in the gallery.

--- src/components/SlideShow.tsx
+++ src/components/SlideShow.tsx
@@ -8,6 +8,8 @@ const SlideShow: React.FC<SlideshowProps> = ({ images }) => {
   // variable d'état avec la valeur initiale définie à 0
   const [ImageIndex, setImageIndex] = useState(0);
 
+
+
   const goToPrevious = () => {
     setImageIndex((prevIndex) =>
       // Si l'indice actuel est supérieur à 0, l'indice diminu de 1, sinon retour à la dernière image du tableau, crée l'effet de boucle.
@@ -21,8 +23,13 @@ const SlideShow: React.FC<SlideshowProps> = ({ images }) => {
       (nextIndex + 1) % images.length);
   };
 
+  console.log( ImageIndex+1, "/", images.length,);
+  
+
   return (
     <div className="slideshow-container">
+       {images.length > 1 && (
+        <>
       <div className="nav-btn">
         <button className="prev-btn" onClick={goToPrevious}>
           <img className="nav-slideshow" src="/gotoprev.svg" alt="Image précédente" />
@@ -30,7 +37,12 @@ const SlideShow: React.FC<SlideshowProps> = ({ images }) => {
         <button className="next-btn" onClick={goToNext}>
           <img className="nav-slideshow" src="/gotonext.svg" alt="Image suivante" />
         </button>
-      </div>
+        </div>
+        <span className="counter">
+        {`${ImageIndex + 1} / ${images.length}`}
+        </span>
+        </>
+      )}
       <img
         className="slideshow-image"
         src={images[ImageIndex]}
